db2c50d1125bb427a7bc5c14816b6d6f41d1e59e
jhs512 <jangka512@gmail.com>
2023-09-19 11:38:31 +0900

    @Transactional ì 과 private ê°의 조합이 의미 없는   이유

--- src/main/java/com/sbs/db/domain/question/service/QuestionService.java
+++ src/main/java/com/sbs/db/domain/question/service/QuestionService.java
@@ -3,28 +3,25 @@ package com.sbs.db.domain.question.service;
 import com.sbs.db.domain.question.entity.Question;
 import com.sbs.db.domain.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
+    @Autowired
+    @Lazy
+    private QuestionService self;
     private final QuestionRepository questionRepository;
 
     public Question write(String subject, String content) {
-        Question question = Question
-                .builder()
-                .subject(subject)
-                .content(content)
-                .build();
-
-        write2(subject, content);
-
-        return questionRepository.save(question);
+        return self.write2(subject, content);
     }
 
     @Transactional
-    private Question write2(String subject, String content) {
+    public Question write2(String subject, String content) {
         Question question = Question
                 .builder()
                 .subject(subject)
