fe44bbe8292633ada7032fd0d5cefba11436361d
jhs512 <jangka512@gmail.com>
2023-09-21 13:00:51 +0900

    complete

--- src/main/java/com/sbs/db/domain/question/repository/QuestionRepositoryImpl.java
+++ src/main/java/com/sbs/db/domain/question/repository/QuestionRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.sbs.db.domain.question.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sbs.db.domain.question.entity.Question;
 import lombok.RequiredArgsConstructor;
@@ -9,22 +10,40 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+import static com.sbs.db.domain.question.entity.QAnswer.answer;
 import static com.sbs.db.domain.question.entity.QQuestion.question;
 
 @RequiredArgsConstructor
 public class QuestionRepositoryImpl implements QuestionRepositoryCustom {
-    private final JPAQueryFactory jpqQueryFactory;
+    private final JPAQueryFactory jpaQueryFactory;
 
     public Page<Question> findByKwV3(String kw, Pageable pageable) {
-        List<Question> questions = jpqQueryFactory
+        // Fetching the content (paginated results)
+        List<Question> questions = findByKwV3SelectFromJoinWhere(kw)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // Fetching the total count
+        long total = findByKwV3SelectFromJoinWhere(kw)
+                .fetchCount();
+
+        return new PageImpl<>(questions, pageable, total);
+    }
+
+    private JPAQuery<Question> findByKwV3SelectFromJoinWhere(String kw) {
+        return jpaQueryFactory
                 .selectDistinct(question)
                 .from(question)
+                .leftJoin(question.author)
+                .leftJoin(question.answers, answer)
+                .leftJoin(answer.author)
                 .where(
                         question.subject.contains(kw)
                                 .or(question.content.contains(kw))
-                )
-                .fetch();
-
-        return new PageImpl<>(questions, pageable, questions.size());
+                                .or(question.author.username.contains(kw))
+                                .or(answer.content.contains(kw))
+                                .or(answer.author.username.contains(kw))
+                );
     }
 }
